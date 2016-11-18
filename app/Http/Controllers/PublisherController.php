5e65b7103ba9130597aa92c31fa4966c84416732
Andy Wendt <andy@awendt.com>
2016-11-18 13:21:39 -0700

    refactored publisher controller to use json response helper function

--- app/Http/Controllers/PublisherController.php
+++ app/Http/Controllers/PublisherController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Publisher\Publisher;
+use App\Publisher\PublisherTransformer;
 use Illuminate\Http\Request;
 
 class PublisherController extends Controller
 {
     public function index()
     {
-        return Publisher::all();
+        $publishers = Publisher::all();
+        return response()->json(transform_collection($publishers, new PublisherTransformer()));
     }
 }
